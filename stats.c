6ee63d5d2b5584c21b9d5e4d78048c0b1395224b
srilatha <srishna.sri@yahoo.com>
2018-04-16 21:14:48 -0600

    adding code

--- stats.c
+++ stats.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ * Copyright (C) 2017 by srilatha bandaru
  *
  * Redistribution, modification or use of this software in source or binary
  * forms is permitted as long as the files maintain this copyright. Users are 
